15ba1ca0b406cb343fbf875ed3e3f6750fa18e78
mincheol. bae <kimmincheol99@gmail.com>
2025-05-11 21:15:32 +0900

    저자 출제 / 02. 배열 제어하기

---
+++ programmers/Array_Control.java
@@ -0,0 +1,19 @@
+// 저자 출제 / 02. 배열 제어하기
+
+package programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Array_Control {
+    private static int[] solution(int[] arr) {
+        Integer[] result = Arrays.stream(arr).boxed().distinct().toArray(Integer[] :: new);
+        Arrays.sort(result, Collections.reverseOrder());
+        return Arrays.stream(result).mapToInt(Integer :: intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] n = {4, 2, 2 ,1, 3, 4};
+        System.out.println(Arrays.toString(solution(n)));
+    }
+}
