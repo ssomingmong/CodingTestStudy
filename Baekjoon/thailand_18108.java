cc0cacb5428758a5da63686d4bea027082a8a8b9
mincheol. bae <kimmincheol99@gmail.com>
2025-05-20 10:43:37 +0900

    18108_1998년생인 내가 태국에서는 2541년생?!

---
+++ Baekjoon/thailand_18108.java
@@ -0,0 +1,11 @@
+//18108_1998년생인 내가 태국에서는 2541년생?!
+
+import java.io.*;
+
+public class thailand_18108 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(y-543);
+    }
+}
