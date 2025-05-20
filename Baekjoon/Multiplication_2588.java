49c4dc07678ed3ffd2da5cf36aadc6f3bc46c583
mincheol. bae <kimmincheol99@gmail.com>
2025-05-20 10:59:20 +0900

    2588_곱셈

---
+++ Baekjoon/Multiplication_2588.java
@@ -0,0 +1,21 @@
+// 2588_곱셈
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Multiplication_2588 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        int b1 = b % 10;
+        int b10 = (b / 10) % 10;
+        int b100 = b / 100;
+
+        System.out.println(a * b1);
+        System.out.println(a * b10);
+        System.out.println(a * b100);
+        System.out.println(a * b);
+    }    
+}
