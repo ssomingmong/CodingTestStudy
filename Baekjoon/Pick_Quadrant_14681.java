98b17b26716579f4dce0db6aa7ab065821100a52
mincheol. bae <kimmincheol99@gmail.com>
2025-05-20 15:38:49 +0900

    14681_사분면 고르기

---
+++ Baekjoon/Pick_Quadrant_14681.java
@@ -0,0 +1,18 @@
+// 14681_사분면 고르기
+
+import java.io.*;
+
+public class Pick_Quadrant_14681 {
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        if(x > 0 && y > 0) System.out.println(1);
+        else if(x < 0 && y > 0) System.out.println(2);
+        else if(x < 0 && y < 0) System.out.println(3);
+        else if(x > 0 && y < 0) System.out.println(4);
+        else System.out.println("error");
+    }
+}
