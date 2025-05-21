8f2fb9898b919a579ce93812dfa5be3d2ad65131
mincheol. bae <kimmincheol99@gmail.com>
2025-05-21 19:50:30 +0900

    1102_A+B-8

---
+++ Baekjoon/Plus8_11022.java
@@ -0,0 +1,24 @@
+// 1102_A+B-8
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Plus8_11022 {
+     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int sum = A + B;
+
+            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + sum);
+        }
+    }
+}
