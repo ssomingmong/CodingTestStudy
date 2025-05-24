587f0dd17c31a6cc0cc4a25ce0368704bb5ca21b
mincheol. bae <kimmincheol99@gmail.com>
2025-05-24 13:01:04 +0900

    Update Remain_3052.java

--- Baekjoon/Remain_3052.java
+++ Baekjoon/Remain_3052.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 public class Remain_3052 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();     
 
         for (int i = 0; i < 10; i++) {
             int num = Integer.parseInt(br.readLine());
