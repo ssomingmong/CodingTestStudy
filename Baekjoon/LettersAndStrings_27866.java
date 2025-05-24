aa124543e6e24f3afaa5b7a31d998abd5e1896de
mincheol. bae <kimmincheol99@gmail.com>
2025-05-24 14:49:15 +0900

    27866_문자와 문자열

---
+++ Baekjoon/LettersAndStrings_27866.java
@@ -0,0 +1,14 @@
+// 27866_문자와 문자열
+
+import java.io.*;
+
+public class LettersAndStrings_27866 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
+
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        System.out.println(S.charAt(i - 1));
+    }
+}
