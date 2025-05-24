bb825f065b11e9785b387fe93877f61995fc6ecf
mincheol. bae <kimmincheol99@gmail.com>
2025-05-24 15:24:07 +0900

    2743_단어길이 재기

---
+++ Baekjoon/String_9086.java
@@ -0,0 +1,23 @@
+// 9086_문자열
+
+import java.util.Scanner;
+
+    public class String_9086 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int T = sc.nextInt();
+            sc.nextLine();
+
+            for(int i = 0; i < T; i++) {
+                String string = sc.nextLine();
+                if(string.length() == 1) {
+                    System.out.println(string + string);
+                }
+                else {
+                    System.out.println(string.charAt(0)+""+ string.charAt(string.length() - 1));
+                }
+            }
+            sc.close();
+        }
+    }
