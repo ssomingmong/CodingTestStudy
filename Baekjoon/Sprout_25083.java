625e0a0c04b56c1bb28b25fbb266ef8c734c19d6
mincheol. bae <kimmincheol99@gmail.com>
2025-05-25 23:38:26 +0900

    25803_새싹

---
+++ Baekjoon/Sprout_25083.java
@@ -0,0 +1,12 @@
+// 25803_새싹
+
+public class Sprout_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
