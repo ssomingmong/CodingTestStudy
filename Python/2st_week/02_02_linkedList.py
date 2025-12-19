075878264c7ab3d2e1be465333ab8aa6d8d4f78f
Mincheol Kim <kimmincheol99@gmail.com>
2025-12-19 17:59:10 +0900

    Create 02_02_linkedList.py

---
+++ Python/2st_week/02_02_linkedList.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+
+    def append(self, value):
+        cur = self.head
+        
+        while cur.next is not None:
+            cur = self.next
+        
+        cur.next = Node(value)
+
+    def print_all(self):
+        cur = self.head
+        while cur is not None:
+            print(cur.data)
+            cur = cur.next
+
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.print_all()
