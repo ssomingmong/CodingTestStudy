fc3224c1c640a82d423a1ce9f43dab9906ecb701
Mincheol Kim <kimmincheol99@gmail.com>
2026-01-27 17:24:49 +0900

    Update app.py

--- jungle_practice/moviestar/app.py
+++ jungle_practice/moviestar/app.py
@@ -66,13 +66,7 @@ def show_movies():
     #    TODO: 개봉일 순서 정렬처럼 여러 기준으로 순서대로 정렬해야되는 경우 sort([('A', 1), ('B', 1)]) 처럼 인자를 설정해야 합니다!!!
     #    TODO: 다음 코드에서 likes로 정렬이 정상동작하도록 직접 수정해보세요!!!
     if sortMode == 'likes':
-        movies = list(db.movies.find({'trashed': False}, {}).sort('likes', -1))
-    
-    elif sortMode == 'viewers':
-        movies = list(db.movies.find({'trashed': False}, {}).sort('viewers', -1))
-
-    elif sortMode == 'date':
-        movies = list(db.movies.find({'trashed': False}, {}).sort([('open_year', -1), ('open_month', -1), ('open_day', -1)]))
+        movies = list(db.movies.find({'trashed': False}, {}).sort("likes", -1)) # 정렬시 여기를 완성해보세요!!!
     else:
         return jsonify({'result': 'failure'})
 
@@ -83,22 +77,9 @@ def show_movies():
 # API #3: 영화에 좋아요 숫자를 하나 올립니다.
 @app.route('/api/like', methods=['POST'])
 def like_movie():
+    movie_id = request.form.get('movie_id')
     # 1. movies 목록에서 find_one으로 영화 하나를 찾습니다.
-    #    TODO: 영화 하나만 찾도록 다음 코드를 직접 수정해보세요!!!
-    movie_id = request.form.get('id')
-    if not movie_id:
-        return jsonify({'result': 'failure'})
-    
-    movie = db.movies.find_one({'_id': ObjectId(movie_id)}) # 여기를 완성 해보세요
-    if not movie_id:
-        return jsonify({'result': 'failure'})
-
-    # 2. movie의 like 에 1을 더해준 new_like 변수를 만듭니다.
-    new_likes = movie['likes'] + 1
-
-    # 3. movies 목록에서 id 가 매칭되는 영화의 like 를 new_like로 변경합니다.
-    #    참고: '$set' 활용하기!
-    result = db.movies.update_one({'_id': ObjectId(movie_id)}, {'$set': {'likes': new_likes}})
+    result = db.movies.update_one({'_id': ObjectId(movie_id)}, {'$inc': {'likes': 1}})
 
     # 4. 하나의 영화만 영향을 받아야 하므로 result.updated_count 가 1이면  result = success 를 보냄
     if result.modified_count == 1:
@@ -106,7 +87,16 @@ def like_movie():
     else:
         return jsonify({'result': 'failure'})
 
+@app.route('/api/trash', methods=['POST'])
+def trash_movie():
+    movie_id= request.form.get('movie_id')
+    result = db.movies.update_one({'_id': ObjectId(movie_id)}, {'$set': {'trashed': True}})
+
+    if result.modified_count == 1:
+        return jsonify({'result':'success'})
+    else:
+        return jsonify({'result':'failure'})
 
 if __name__ == '__main__':
     print(sys.executable)
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
